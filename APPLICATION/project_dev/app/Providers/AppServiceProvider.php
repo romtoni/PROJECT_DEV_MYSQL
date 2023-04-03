d6ca87de34d15dda3aa969cb8d703883623be6a2
ROMTONI <ROMTONI@BHINNEKALIFE.COM>
2023-04-03 10:50:31 +0700

    10:05 03/04/2023 - Installing Bootstrap 5 - Remove former of sample register and login page

--- APPLICATION/project_dev/app/Providers/AppServiceProvider.php
+++ APPLICATION/project_dev/app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
-
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,7 +20,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-		Schema::defaultStringLength(191);
-
     }
 }
